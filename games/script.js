9f1dad2c66bdc1e1804864efdaa9e5b056c59047
cobalt61 <aramzar@icloud.com>
2025-04-29 15:54:29 -0700

    Update script.js

--- games/script.js
+++ games/script.js
@@ -4,7 +4,7 @@ function decodeBase64(str) {
   return atob(str);
 }
 
-fetch('games/password.json')
+fetch('/games/password.json')
   .then(response => response.json())
   .then(data => {
     correctPassword = decodeBase64(data.password);
